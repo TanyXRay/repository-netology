e9e769f5555fbd8c520dfe9eecb218952e302e1b
Tatyana Chernyadieva <tany.taray@gmail.com>
2022-07-20 22:50:22 +0400

    add method averageSalesValue

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         SalesManager first = new SalesManager(new int[]{10, 500, 64, 79,945});
         first.max();
+        first.averageSalesValue();
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -5,6 +5,10 @@ public class SalesManager {
         this.sales = sales;
     }
 
+    /**
+     * Данный метод возвращает максимальное значение по продажам.
+     * @return long max (максимальное значение).
+     */
     public int max() {
         int max = -1;
         for (int sale : sales) {
@@ -13,5 +17,21 @@ public class SalesManager {
             }
         }
         return max;
+    }
+
+    /**
+     * Данный метод вычисляет среднее значение по продажам, за исключением max и min значений.
+     * @return  int mean (среднее значение).
+     */
+    public int averageSalesValue() {
+        int sum = 0;
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+            sum += sale;
+        }
+        return (sum - min - max()) / (sales.length - 2);
     }
 }
