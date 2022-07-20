efaed1b61c89078d781e5ebab0d7dd2aec708974
Tatyana Chernyadieva <tany.taray@gmail.com>
2022-07-20 22:45:35 +0400

    change to long

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        SalesManager first = new SalesManager(new int[]{10, 500, 64, 79,945});
+        SalesManager first = new SalesManager(new long[]{10, 500, 64, 79,945});
         first.max();
     }
 }
